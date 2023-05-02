554644a1e28261969874b3b51cb13eb6bb3df8e2
mar-babu <ar_cse@yahoo.com>
2023-05-02 13:41:02 +0600

    defining scopes

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -32,5 +32,9 @@ class AuthServiceProvider extends ServiceProvider
         Passport::tokensExpireIn(now()->addDays(1));
         Passport::refreshTokensExpireIn(now()->addDays(30));
         Passport::personalAccessTokensExpireIn(now()->addMonths(6));
+
+        Passport::tokensCan([
+            'view-user' => 'View User Information'
+        ]);
     }
 }
