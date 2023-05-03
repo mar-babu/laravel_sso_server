b9033624559a848b0af470f62a8641675dc7f9bc
mar-babu <ar_cse@yahoo.com>
2023-05-03 17:31:30 +0600

    stop auto authorize and switch account

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use \Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api', 'scope:view-user')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:api')->get('/logmeout', function (Request $request) {
+    $user = $request->user();
+    $accessToken = $user->token();
+    DB::table('oauth_refresh_tokens')
+        ->where('access_token_id', $accessToken->id)->delete();
+    $accessToken->delete();
+    return response()->json([
+        'message' => 'revoked'
+    ]);
+});
+
